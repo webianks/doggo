d111f04646a89591c1dec5eb8342e628833cb4a3
webianks <webianks@gmail.com>
2021-02-27 18:14:18 +0530

    Add licences

--- app/src/main/java/com/example/androiddevchallenge/ui/pages/Details.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/pages/Details.kt
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.pages
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
@@ -23,7 +37,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -100,33 +113,6 @@ private fun Details(doggo: Doggo) {
             Spacer(modifier = Modifier.width(10.dp))
             Text(
                 text = "Adopt Now".toUpperCase(Locale.getDefault())
-            )
-        }
-    }
-}
-
-@Composable
-private fun InfoItem(title: String, value: String) {
-    Column(
-        modifier = Modifier
-            .defaultMinSize(
-                minWidth = 80.dp
-            )
-            .padding(10.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = title,
-            color = MaterialTheme.colors.onPrimary,
-            style = MaterialTheme.typography.subtitle1.copy(fontWeight = FontWeight.Medium)
-        )
-        Spacer(modifier = Modifier.height(5.dp))
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text(
-                text = value,
-                color = MaterialTheme.colors.onPrimary,
-                style = MaterialTheme.typography.subtitle2.copy(fontWeight = FontWeight.Normal)
             )
         }
     }
